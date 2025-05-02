c144256d25f5138c5f0b432bcf90d63922cc9f0a
amehmathias <amehmathiasejeh40@gmail.com>
2025-05-02 22:43:52 +0100

    new commit

--- my_module/calculator.js
+++ my_module/calculator.js
@@ -14,8 +14,3 @@ export function divide(a, b) {
   return a / b;
 }
 
-
-// console.log(add(5, 3));
-// console.log(subtract(5, 3));
-// console.log(multiply(5, 3));
-// console.log(divide(5, 3).toFixed(2)); // 1.67
