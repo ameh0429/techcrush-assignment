434863a488d153e179676706bfc93810816cc74a
amehmathias <amehmathiasejeh40@gmail.com>
2025-05-05 10:33:06 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ import{add, subtract, multiply, divide}from './my_module/calculator.js';
 // Accessing metadata about the module
 console.log(chalk.yellow (import.meta.url)); 
 
-console.log(chalk.red (`Addition is: ${add(5, 3)}`));
-console.log(chalk.green (`Subtraction is: ${subtract(5, 3)}`));
-console.log(chalk.blue (`Multiplication is: ${multiply(5, 3)}`));
-console.log(chalk.magenta (`Division is: ${divide(5, 3).toFixed(2)}`)); // 1.67
+const addResult = chalk.red (`Addition is: ${add(5, 3)}`);
+const subtractResult = chalk.green (`Subtraction is: ${subtract(5, 3)}`);
+const multiplyResult = chalk.blue (`Multiplication is: ${multiply(5, 3)}`);
+const divideResult = chalk.magenta (`Division is: ${divide(5, 3).toFixed(2)}`);
+
+console.log(addResult);
+console.log(subtractResult);
+console.log(multiplyResult);
+console.log(divideResult);
 
